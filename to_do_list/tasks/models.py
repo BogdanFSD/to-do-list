74e2850f8b3fa05f77c077b37b43c4c1c1405e9c
Bogdandev <luitko007@gmail.com>
2023-08-01 05:16:36 +0000

    Created models, added static and templates folder

--- to_do_list/tasks/models.py
+++ to_do_list/tasks/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+
+class Tag(models.Model):
+    title = models.CharField(max_length=255, unique=True)
+
+    def __str__(self):
+        return self.title
+
+
+class Task(models.Model):
+    content = models.CharField(max_length=255)
+    created = models.DateTimeField(auto_now_add=True)
+    deadline = models.DateTimeField(blank=True, null=True)
+    performance = models.BooleanField(default=False)
+    tags = models.ManyToManyField(Tag)
+
+    class Meta:
+        ordering = ("-performance", "-created")
+
+    def __str__(self):
+        return self.content
