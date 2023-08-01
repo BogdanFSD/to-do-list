5c5cbdf480e8918bf3f2e264d56d91c5754281c7
Bogdandev <luitko007@gmail.com>
2023-08-01 05:55:20 +0000

    Created tag delete page

--- to_do_list/tasks/views.py
+++ to_do_list/tasks/views.py
@@ -26,3 +26,9 @@ class TagUpdateView(generic.UpdateView):
     model = Tag
     form_class = TagForm
     success_url = reverse_lazy("tasks:tag_list")
+
+
+class TagDeleteView(generic.DeleteView):
+    model = Tag
+    success_url = reverse_lazy("tasks:tag_list")
+
